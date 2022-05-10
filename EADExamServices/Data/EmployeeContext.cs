5e409a8c4e9345a97c2ff5b1ece449a9cb4810eb
Bui Thanh Dat <datbtth2011018@fpt.edu.vn>
2022-05-10 22:31:28 +0700

    first commit

---
+++ EADExamServices/Data/EmployeeContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using EADExamServices.Entity;
+
+namespace EADExamServices.Data
+{
+    public class EmployeeContext: DbContext
+    {
+        public EmployeeContext() : base("name=EADExam")
+        {
+        }
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
---
+++ EADExamServices/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace EADExamServices.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EADExamServices.Data.EmployeeContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(EADExamServices.Data.EmployeeContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
